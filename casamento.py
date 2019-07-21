b38ee04d179048eeb04323a7ccc7951d76f87f68
theo <lucastheo.g.a@gmail.com>
2019-07-21 17:24:53 -0300

    arrumando problemas e adicioando funcoes

--- casamento.py
+++ casamento.py
@@ -1,6 +1,6 @@
 
 import Levenshtein
-from nome import Nome
+from .nome import Nome
 
 def distanciaString( nome0:str, nome1:str):
     """
@@ -14,7 +14,7 @@ def distanciaNome( nome0 , nome1 ):
     """
     flagNomeReduzido = False
     valor = 0
-    if len( nome0.listNome ) > 1 and len( nome0.listNome ) > 1:
+    if len( nome0.listNome ) > 1 and len( nome1.listNome ) > 1:
         #caso for maior que 1
         for apo0 in nome0.listNomeGrandes:
             for apo1 in nome1.listNomeGrandes:
@@ -22,8 +22,11 @@ def distanciaNome( nome0 , nome1 ):
                     valor = casouUmNome( nome0, nome1, apo0, apo1 )
     else:
         #caso for menor que 1 ( nome unico ) 
-        if existeNomeIgual( nome0.listNome, nome0.listNome ) == True:
-            valor = 0.3
+        if existeNomeIgual( nome0.listNome, nome1.listNome ) == True:
+            if len( nome0.listNome ) == 1 and len( nome1.listNome ) == 1:
+                valor = 0.7
+            else:
+                valor = 0.3
     return valor
 
 def esseNomesSaoIguais( nome0, nome1 ):
@@ -82,7 +85,12 @@ def quantidadePrimeiraLetraIgual( lista0:list, lista1:list):
                 break
     return cont
 
-
+def nomeEstaNesseGrupo( nomeCompara, grupo ):
+    nomeComparaBase = nomeCompara.base
+    for nomeGrupo in grupo:
+        if nomeComparaBase == nomeGrupo.base:
+            return True
+    return False
 
 
 class CasamentoNome:
@@ -96,15 +104,15 @@ class CasamentoNome:
         """
             julga dando o melhor caso que tem armazenado
         """
+       
         nomeBase = nome.base
-        if nomeBase in self.hashCasamento:
-            index = self.hashCasamento[ nome ]
-            return self.grupos[ index ]
-        
+        if nomeBase in self.hashCasamento.keys():
+            index = self.hashCasamento[ nomeBase ]
+            return [ index, 1 ]
         """
             Tenta encontrar o melhor caso entre os diversos pela média dos casos próximos
         """
-        maximo = -1     #indice do maior elemento
+        maximoLocal = -1     #indice do maior elemento
         valorMaximo = 0 #valor do indice maximo
         valor = 0       #valor para comparar
 
@@ -114,15 +122,16 @@ class CasamentoNome:
             #aplicando a média
             valor = 0
             for apoNome in grupo:
-                valor += distanciaNome( nome, apoNome )            
+                apoSoma = distanciaNome( nome, apoNome )             
+                valor += apoSoma
             valor = valor / len( grupo )
             # fim da média 
 
             if valor > valorMaximo: #trocando os valores pelos novos
                 valorMaximo = valor     
-                maximo = i
-            
-        return [ maximo, valorMaximo ]
+                maximoLocal = i
+        
+        return [ maximoLocal, valorMaximo ]
 
     def julgandoAdicioandoPorObjNome( self, nome, parametro = 0.2 ):
         """
@@ -134,8 +143,8 @@ class CasamentoNome:
         apo = self.__julgando( nome )
         maximo = apo[ 0 ]
         valorMaximo = apo[ 1 ]
-            
-        if valorMaximo < parametro:
+
+        if maximo == -1 or valorMaximo < parametro:
             """
                 caso não passar o valor de parametro ele gera um novo grupo, ou seja,
                 um novo nome, esse algoritmo é bem lento para adicionar nome novos.
@@ -143,20 +152,58 @@ class CasamentoNome:
             maximo =  len( self.grupos )    
             nomeBase = nome.base
             self.hashCasamento[ nomeBase ] = maximo
-            self.grupos.append( [nome ] )
+
+            self.grupos.append( set() )
+            self.grupos[ maximo ].add( nome )
         else:
-            self.grupos[ maximo ].append( nome )
+            grupo = self.grupos[ maximo ]
+            if nomeEstaNesseGrupo( nome , grupo ) == False:
+                self.hashCasamento[ nome.base ] = maximo
+                grupo.add( nome )
         return maximo
 
     def julgandoAdicioandoPorStr( self, nome:str, parametro = 0.2 ):
+        """
+            pega a string e já gera o objStr
+        """
         apo = Nome( nome )
         return self.julgandoAdicioandoPorObjNome( apo , parametro)
         
 
     def julgandoPorObj( self , nome ):
+        """
+            retorna o grupo pelo nome
+        """
         apo = self.__julgando( nome )
         return apo[ 0 ] 
     def julgandoPorStr( self, nome:str):
-        apo = Nome( nome )
-        self.julgandoPorObj( apo )
-        return apo[ 0 ]
+        """
+            retorna o grupo pelo obj que foi gerado no momento
+        """
+        nomeApo = Nome( nome )
+        apo = self.julgandoPorObj( nomeApo )
+        return apo
+    
+    def getNome( self, index:int ):
+        """
+            Pega o maior nome do grupo apartir do id
+        """
+        grupo = self.grupos[ index ]
+        maiorNome = ""
+        for objNome in grupo:
+            nomeBase = objNome.base
+            if len( maiorNome ) < len( nomeBase ):
+                maiorNome = nomeBase
+        return maiorNome
+    
+    def getTodosOsNomes( self, index:int ):
+        """
+            Pega o maior nome do grupo apartir do id
+        """
+        grupo = self.grupos[ index ]
+        maiorNome = "["
+        for objNome in grupo:
+            nomeBase = objNome.base
+            #if len( maiorNome ) < len( nomeBase ):
+            maiorNome += nomeBase + ";"
+        return maiorNome.rstrip(";") + "]"
--- nome.py
+++ nome.py
@@ -1,24 +1,28 @@
+vogais = {"a", "e", "o", "i", "u"}
 class Nome:
     '''
         Pensado para fazer analises com nomes
     '''
     def __init__(self, name:str):
         # armazena a base
+        name = name.replace(".", "")
+        self.base = name
+        #armazena a informacao do priemiro nome
+        self.listPrimeirasLetras = list()
+        self.listNome = list()
+        self.listNomeGrandes = list()
+        self.__casamento = list()
         if len( name ) > 0:
-            self.base = name
-            #armazena a informacao do priemiro nome
-            self.listPrimeirasLetras = list()
-            self.listNome = list()
-            self.listNomeGrandes = list()
-            self.__casamento = list()
-
             self.inicio()
 
-
+    def __str__(self):
+        return f"[{self.base}]"
 
     def inicio(self):
         #cortar o nome em partes pelo " "
         apo = self.base.lower()
+        apo = apo.replace("  ", "")
+        apo = apo.strip(" ")
         if " " in apo:
             self.listNome = apo.split(' ')
         else:
@@ -27,6 +31,11 @@ class Nome:
         for ele in self.listNome:
             if len( ele ) > 1:
                 self.listNomeGrandes.append( ele )
+            elif len( ele ) == 2:
+                if vogais not in ele:
+                    self.listNomeGrandes.append( ele[ 0 ])
+                    self.listNomeGrandes.append( ele[ 1 ])
+
 
         for ele in self.listNome:
             self.listPrimeirasLetras.append( ele[ 0 ])
